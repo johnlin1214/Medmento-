66f8ff3da1bf877b5d9a34273019914cbb64be10
[01;31m[K"name"[m[K: "John Lin <[01;31m[K"email"[m[K: "johnlin1214@gmail.com>
2015-04-05 13:14:22 -0700

    added verification feature

--- app/assets/javascripts/demo.js
+++ app/assets/javascripts/demo.js
@@ -1,6 +1,25 @@
-$(function(){
-  console.log("hello")
-  $('.attendant-form')
+$(function() {
+  $('#attendant_npi').on('keyup', validateNpiNumber);
+});
 
-})
-attendant_npi
+function validateNpiNumber() {
+    $('#npi-confrim').html("<p id='npi-confrim'>Please Enter A Valid NPI Number</p>")
+    var npiNumber = $('#attendant_npi').val();
+    if (npiNumber.length === 10) {
+        $('#npi-confrim').html("<p id='npi-confrim'>Please Enter A Valid NPI Number</p>")
+        $.ajax({
+          type: 'GET',
+          url: 'http://www.bloomapi.com/api/sources/usgov.hhs.npi/' + npiNumber,
+          dataType: 'jsonp'
+        }).done(function(server_data) {
+          if (server_data) {
+            $('#npi-confrim').text("NPI Number Found").css("color", "green");
+          }
+        });
+    };
+}
+
+
+
+// 1013903475
+// 101390347
