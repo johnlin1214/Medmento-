7b43b4d5d4629cd9459dcd84c2bc5da3ff7c3a01
[01;31m[K"name"[m[K: "John Lin <[01;31m[K"email"[m[K: "johnlin1214@gmail.com>
2015-04-09 19:21:29 -0700

    paresed JSON data from name lookup

--- app/assets/javascripts/demo.js
+++ app/assets/javascripts/demo.js
@@ -1,6 +1,6 @@
 $(function() {
   $('#attendant_npi').on('keyup', validateNpiNumber);
-  lookUpNpiNumber();
+  $('#npi-name-lookup').on('submit', lookUpNpiNumber)
 });
 
 function validateNpiNumber() {
@@ -20,9 +20,12 @@ function validateNpiNumber() {
     };
 }
 
-function lookUpNpiNumber(last, first) {
-  last = 'LIN'
-  first = 'JOHN'
+function lookUpNpiNumber(evt) {
+  evt.preventDefault();
+  // var first = $('#npi-name-lookup').find('input[name="firstname"]').val();
+  // var last = $('#npi-name-lookup').find('input[name="lastname"]').val();
+  var first = "SHADI"
+  var last = "HAMIDKHANI"
   $.ajax({
     type: 'GET',
     url: 'http://www.bloomapi.com/api/search?offset=0&key1=last_name&op1=eq&value1=' + last + '&key2=first_name&op2=eq&value2=' + first,
@@ -30,6 +33,16 @@ function lookUpNpiNumber(last, first) {
     cache: true
   }).done(function(server_data) {
     console.log(server_data)
+    server_data.result.forEach(function(data) {
+      console.log(data.first_name)
+      console.log(data.last_name)
+      console.log(data.practice_address.address_line)
+      console.log(data.practice_address.city)
+      console.log(data.practice_address.state)
+      console.log(data.practice_address.zip)
+      console.log(data.practice_address.phone)
+      $('#search-table tr:last').append("<h2>Hello<h2>")
+    })
   }).fail(console.log("failed"));
 }
 // Failed to load resource: the server responded with a status of 400 (Bad Request)
