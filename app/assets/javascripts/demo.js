861aecaf752748e44e9fb2ff578d9234dbe15941
[01;31m[K"name"[m[K: "John Lin <[01;31m[K"email"[m[K: "johnlin1214@gmail.com>
2015-04-10 17:34:13 -0700

    fixed styling bug for npi lookup

--- app/assets/javascripts/demo.js
+++ app/assets/javascripts/demo.js
@@ -1,6 +1,7 @@
 $(function() {
   $('#attendant_npi').on('keyup', validateNpiNumber);
-  $('#npi-name-lookup').on('submit', lookUpNpiNumber)
+  $('#npi-name-lookup').on('submit', lookUpNpiNumber);
+  $('#search-table').on('click', '.npi-select', addSelectedNPInumber)
 });
 
 function validateNpiNumber() {
@@ -31,23 +32,18 @@ function lookUpNpiNumber(evt) {
     cache: true
   }).done(function(server_data) {
     var results = server_data.result.sort(function(a,b){return a.practice_address - b.practice_address })
-    debugger
     results.forEach(function(data) {
-      console.log(data.first_name)
-      console.log(data.last_name)
-      console.log(data.practice_address.address_line)
-      console.log(data.practice_address.city)
-      console.log(data.practice_address.state)
-      console.log(data.practice_address.zip)
-      var searchResultRow = "<tr><td>"+ data.first_name + " " + data.last_name +"</td><td>" + data.practice_address.address_line + "</td><td>"+ data.practice_address.city +"</td><td>"+ data.practice_address.state +"</td><td>"+ data.npi +"</td></tr>";
+      var searchResultRow = "<tr><td>"+ data.first_name + " " + data.last_name +"</td><td>" + data.practice_address.address_line + "</td><td>"+ data.practice_address.city +"</td><td>"+ data.practice_address.state +"</td><td class='npi-result'>"+ data.npi +"</td><td><button type='button' class='btn btn-success btn-xs npi-select'>Select</button></td></tr>";
       $("#search-table tr:last").after(searchResultRow);
     })
   }).fail(console.log("failed"));
 }
-// Failed to load resource: the server responded with a status of 400 (Bad Request)
 
-// GET http://www.bloomapi.com/api/search/npi?limit=10&offset=0&key1=last_name&op1=eq&value1=lin&key2=first_name&op2=eq&value2=john
-// http://www.bloomapi.com/api/search/npi?limit=10&offset=0&key1=last_name&op1=eq&value1=jansa&key2=first_name&op2=eq&value2=galeb
-
+function addSelectedNPInumber() {
+  var npiSelected = $(this).closest('tr').find(".npi-result").text();
+  console.log(npiSelected);
+  $('#attendant_npi').val(npiSelected);
+  $('#npi-confrim').text("NPI Number Found").css("color", "green");
+}
 // 1013903475
 // 101390347
