236af55b41aea1a71bd1cef655f8337aaee60276
[01;31m[K"name"[m[K: "John Lin <[01;31m[K"email"[m[K: "johnlin1214@gmail.com>
2015-04-15 17:26:00 -0700

    updated readme

--- app/assets/javascripts/demo.js
+++ app/assets/javascripts/demo.js
@@ -5,10 +5,10 @@ $(function() {
 });
 
 function validateNpiNumber() {
-    $('#npi-confrim').html("<p id='npi-confrim'>Please Enter A Valid NPI Number</p>")
+    $('#npi-confrim').html("<p id='npi-confrim'>Enter A Valid NPI Number</p>")
     var npiNumber = $('#attendant_npi').val();
     if (npiNumber.length === 10) {
-        $('#npi-confrim').html("<p id='npi-confrim'>Please Enter A Valid NPI Number</p>")
+        $('#npi-confrim').html("<p id='npi-confrim'>Enter A Valid NPI Number</p>")
         $.ajax({
           type: 'GET',
           url: 'http://www.bloomapi.com/api/sources/usgov.hhs.npi/' + npiNumber,
